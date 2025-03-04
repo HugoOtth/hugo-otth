0220ccc50129bd6e221c39547a4f8eba7f259f72
HugoOtth <hugo.otth@gmail.com>
2025-03-04 18:09:09 -0500

    Redo next.js 2

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 
-module.exports = {
-  output: 'export',
-  basePath: '/hugo-otth',
-  assetPrefix: process.env.MODE_ENV === 'production' ? 'https://www.hugo-otth.com' : '',
-}
-
 const nextConfig: NextConfig = {
   /* config options here */
 };
